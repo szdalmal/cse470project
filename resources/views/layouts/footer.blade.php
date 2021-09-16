86b59a1a3fd3c8d366d558f645bee5ca5463883a
szdalmal <sazzadalamtomalforpublic@gmail.com>
2021-09-17 00:12:35 +0600

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<!--Footer-->
+<footer id="footer" class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+        <!--Copyright-->
+        <div class="footer-text">
+            <a href="https://bux.bracu.ac.bd/courses/course-v1:buX+CSE470+2021_Summer/course/"> For CSE470</a>
+        </div>
+        <!--/.Copyright-->
+</footer>
+<!--/.Footer-->
